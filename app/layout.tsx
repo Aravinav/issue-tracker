4513ac3730ac07050da137dac31dc831fd03ffad
aravind <aravind.baskaran@inspirisys.com>
2023-12-26 12:40:28 +0530

    Add a mark down editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import '@radix-ui/themes/styles.css';
+import './theme-config.css'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-import '@radix-ui/themes/styles.css';
 import { Theme, ThemePanel } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
@@ -23,7 +24,6 @@ export default function RootLayout({
       <Theme appearance="light" accentColor="purple">
         <NavBar/>
         <main className='p-5'>{children}</main>
-        <ThemePanel/>
         </Theme>
         </body>
     </html>
